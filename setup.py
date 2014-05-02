8f116e618886d466736d867b19784b486c01d061
akunze3 <Andreas.Kunze@mailbox.tu-dresden.de>
2014-05-02 14:38:27 +0200

    still trying

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ This file is part of PyTrajectory.
 
 #! /usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 import codecs
 
@@ -20,12 +20,16 @@ long_description = read('README.rst')
 setup(name='PyTrajectory',
     #version=pytrajectory.__version__,
     version='0.3.3',
+    #packages=['pytrajectory'],
+    packages=find_packages()
+    
     install_requires=['numpy>=1.8.1',
                     'sympy>=0.7.5',
                     'scipy>=0.13.3'],
+    
+    # metadata for upload to PyPI
     author='Andreas Kunze, Carsten Knoll, Oliver Schnabel',
     author_email='Andreas.Kunze@mailbox.tu-dresden.de',
     description='Python library for trajectory planning.',
-    long_description=long_description,
-    packages=['pytrajectory']
+    long_description=long_description
     )
