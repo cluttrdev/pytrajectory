392d3bada36ef7fe98924c846c00778289fbe1fe
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2015-01-09 20:18:27 +0100

    changed scipy version in setup dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='PyTrajectory',
     packages=['pytrajectory'],
     requires=['numpy (>=1.8.1)',
                 'sympy (>=0.7.5)',
-                'scipy (>=0.13.3)',
+                'scipy (>=0.13.0)',
                 'matplotlib'],
     
     # metadata
