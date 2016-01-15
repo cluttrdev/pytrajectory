22f91de4f00446d35b3fedb145bfc8b1cb73f1da
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-01-15 13:34:03 +0000

    adapted setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(name='PyTrajectory',
     PyTrajectory is a Python library for the determination of the feed forward 
     control to achieve a transition between desired states of a nonlinear control system.
     ''',
-      setup_requires=['numpy>=1.8.1', 'sympy>=0.7.5', 'scipy>=0.13.0', 'matplotlib<1.5.0'],
-      install_requires=['numpy>=1.8.1', 'sympy>=0.7.5', 'scipy>=0.13.0', 'matplotlib<1.5.0']
+      #setup_requires=['numpy>=1.8.1', 'sympy>=0.7.5', 'scipy>=0.13.0', 'matplotlib<1.5.0'],
+      #install_requires=['numpy>=1.8.1', 'sympy>=0.7.5', 'scipy>=0.13.0', 'matplotlib<1.5.0']
     )
